e5b77b10bb34d04961dcbc5a6c6cfefdfcbdc388
petrak@ <petrathekat@gmail.com>
2023-08-02 21:27:35 -0500

    make cislocationals more valuable and actually install all the files

--- install.py
+++ install.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-import subprocess, glob, itertools
+import subprocess
+import glob
+import itertools
 
-files = ["script", "manifest.json", "*.xml"]
+files = ["script", "*.json", "*.xml", "thumbnail.png"]
 mod_name = "petrakat_yank"
 
 coq_dir = "/home/petrak/.config/unity3d/Freehold Games/CavesOfQud/Mods/"
@@ -11,4 +13,5 @@ coq_dir = "/home/petrak/.config/unity3d/Freehold Games/CavesOfQud/Mods/"
 files = [glob.glob(f) for f in files]
 subprocess.call(["rm", "-r", coq_dir + mod_name])
 subprocess.call(["mkdir", coq_dir + mod_name])
-subprocess.call(["cp", "-r"] + list(itertools.chain(*files)) + [coq_dir + mod_name])
+subprocess.call(
+    ["cp", "-r"] + list(itertools.chain(*files)) + [coq_dir + mod_name])
